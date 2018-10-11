6d65564417f694443cd8516affa00fd1675be0e2
paveldrvn <php5.drvn@gmail.com>
2018-10-12 01:55:48 +0300

    add nuxt with bootsrap preset; nuxt v 140

---
+++ admin/view/javascript/d_visualize/nuxt/middleware/user-agent.js
@@ -0,0 +1,5 @@
+export default function (context) {
+    // window.history.pushState("object or string", "Title", window.location.href + "#" + context.base);
+    // Add the userAgent property in the context (available in `asyncData` and `fetch`)
+    // console.log(context.base)
+}
