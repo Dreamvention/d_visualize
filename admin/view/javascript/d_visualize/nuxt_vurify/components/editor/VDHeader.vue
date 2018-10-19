b2f58a3387f5f5c64c3f355ee0bebc512bd633f5
pavel <php5.drvn@gmail.com>
2018-10-19 18:05:41 +0300

    work on editor

--- admin/view/javascript/d_visualize/nuxt_vurify/components/editor/VDHeader.vue
+++ admin/view/javascript/d_visualize/nuxt_vurify/components/editor/VDHeader.vue
@@ -6,7 +6,7 @@
 
 <script>
 	export default {
-		name: "NavigationContext"
+		name: "VDHeader"
 	};
 </script>
 
--- admin/view/javascript/d_visualize/nuxt_vurify/pages/editor/index.vue
+++ admin/view/javascript/d_visualize/nuxt_vurify/pages/editor/index.vue
@@ -1,12 +1,44 @@
 <template>
     <div class="sections">
-        sections
+        <v-list >
+            <template v-for="(item, index) in items">
+                <v-list-tile
+                        :key="item.title"
+                        @click=""
+                >
+                    <v-list-tile-content>
+                        <v-list-tile-title >{{item.title}} <v-icon>fas fa-chevron-right</v-icon></v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+                <v-divider></v-divider>
+
+            </template>
+        </v-list>
     </div>
 </template>
 
 <script>
+    import VDHeader from '~/components/editor/VDHeader'
 	export default {
-		name: "index"
+		name: "index",
+        components:{
+	        VDHeader
+        },
+		data () {
+			return {
+				items: [
+					{
+						title: 'Header',
+					},
+					{
+						title: 'Summer BBQ ',
+					},
+					{
+						title: 'Footer',
+					}
+				]
+			}
+		}
 	};
 </script>
 
